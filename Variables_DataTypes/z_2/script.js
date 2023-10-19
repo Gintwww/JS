aa3f86a91b0090f8476ded3a5863acb501fdb570
Chtholly <A.pupyev@inbox.ru>
2023-10-19 22:14:59 +0700

    check_work

---
+++ Variables_DataTypes/z_2/script.js
@@ -0,0 +1,47 @@
+const checkButton = document.getElementById("checkButton");
+
+checkButton.addEventListener("click", function()
+{
+  const wordInput = document.getElementById("numberInput");
+
+  if (isNaN(wordInput))
+  {
+    const resultElement = document.getElementById("rezult");
+
+
+
+  }
+
+  function separator(str)
+  {
+    const words = str.split(' ');
+    const wordList = [];
+
+    for (let word of words)
+    {
+      wordList.push(word);
+    }
+    
+    return wordList;
+  }
+
+  console.log(separator)
+
+}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
