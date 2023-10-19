95f162b4aba3a55dfe225c29d7c4ae1ed46bfbba
Chtholly <A.pupyev@inbox.ru>
2023-10-19 22:43:16 +0700

    z_2

--- Variables_DataTypes/z_2/script.js
+++ Variables_DataTypes/z_2/script.js
@@ -1,47 +1,11 @@
-const checkButton = document.getElementById("checkButton");
-
-checkButton.addEventListener("click", function()
-{
-  const wordInput = document.getElementById("numberInput");
-
-  if (isNaN(wordInput))
-  {
-    const resultElement = document.getElementById("rezult");
-
-
-
-  }
-
-  function separator(str)
-  {
-    const words = str.split(' ');
-    const wordList = [];
-
-    for (let word of words)
-    {
-      wordList.push(word);
-    }
-    
-    return wordList;
-  }
-
-  console.log(separator)
-
+function reverseWords(str) {
+  const words = str.split(' ');
+  const reversedWords = words.map(word => word.split('').reverse().join(''));
+  return reversedWords.join(' ');
 }
-)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById("reverseButton").addEventListener("click", function () {
+  const inputString = document.getElementById("inputString").value;
+  const reversedString = reverseWords(inputString);
+  document.getElementById("output").textContent = reversedString;
+});
