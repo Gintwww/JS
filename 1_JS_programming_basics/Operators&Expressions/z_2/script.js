1370e74ccfd9616b4ae3282f8294228f28f46a04
Chtholly <A.pupyev@inbox.ru>
2023-10-20 02:03:55 +0700

    z_2

--- 1_JS_programming_basics/Operators&Expressions/z_2/script.js
+++ 1_JS_programming_basics/Operators&Expressions/z_2/script.js
@@ -2,43 +2,47 @@ const checkButton = document.getElementById("rezultButton");
 
 checkButton.addEventListener("click", function()
 {
-  const expression = document.getElementById("inputString").value;
-  const result = calculate(expression);
+  const userInput = document.getElementById("inputString").value;
 
-  document.getElementById("output").textContent = result;
+  const arrayInput = userInput.split(' ');
+  const numberArray = [];
 
+  for (let number of arrayInput)
+  {
+    numberArray.push(number);
+  }
 
+  const rezult = Multiplication_Numbers(numberArray);
 
+  document.getElementById("output").textContent = rezult;
 }
 )
-//Обрабочик ошибки
-function calculate(expression)
+
+function Multiplication_Numbers(array)
 {
-  try 
+  try
   {
-      const result = eval(expression);
-      if (isFinite(result))
-      {
-        return result;
-      }
-      else
-      {
-        throw new Error(' неверное выражение');
-      }
+    if (array.length > 2)
+    {
+      const sorted_array = array.slice().sort((a,b) => a - b);
+      const length_arr = sorted_array.length;
+
+      max_1 = sorted_array[length_arr - 1]
+      max_2 = sorted_array[length_arr - 2]
+
+      return max_1 * max_2;
+    }
+    else
+    {
+      return "Массив должен содержать как минимум 2 элемента";
+    }
+  }
+  catch
+  {
+    return 'ОШИБКА!!!' + error.message;
   }
 
-  catch (error) 
-  {
-    return 'Ошибка вы дебик ';
-  }
 }
-
-
-
-
-
-
-
 
 
 
